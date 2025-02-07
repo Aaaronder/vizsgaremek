a5e1f8d70ed4a96b87bb83eb5cf8d80068d15532
alper fatih <alperfatihcelik@gmail.com>
2025-02-07 22:45:34 +0100

    Add files via upload

--- src/config/db.js
+++ src/config/db.js
@@ -1,26 +1,58 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise'; 
 import dotenv from 'dotenv';
 
 // Betölti a .env fájlban lévő változókat
 dotenv.config(); 
 
 // Adatbázis kapcsolat létrehozása
-const dbConnection = mysql.createConnection({
-    port: process.env.PORT,
-    host: process.env.DB_HOST,
+const dbConnection = await mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DATABASE
 });
 
 dbConnection.connect((err) => {
     if (err) {
-        console.error('Error connection to MySql server: ' + err.stack);
+        console.error('Error connecting to the MySQL server: ' + err.stack);
         return;
     }
-    console.log('Successfully connected to MySql server');
+    console.log('Connected to the MySQL server.');
 });
 
-const promisePool = dbConnection.promise();
+/*export async function getUsers() {
+    let sql = 'SELECT * FROM users';
+    const [result] = await dbConnection.execute(sql);
+    return result;
+};*/
 
-export default promisePool;
+export async function getUser(id) {
+    let sql = 'SELECT * FROM users WHERE userId = ?';
+    const [result] = await dbConnection.execute(sql, [id]);
+    return result;
+}
+
+export async function insertUser(user) {
+    let sql = 'INSERT INTO users (userId, userName, userEmail, userPassword, userPp, userCreated) VALUES (NULL, ?, ?, ?, ?, ?)';
+    const [result] = await dbConnection.execute(sql, [user.userName, user.Email, user.Password, user.Pp, user.Created]);
+    return result;
+};
+
+export async function loginUser(user) {
+    let sql = 'SELECT * FROM users WHERE userName = ? AND userPassword = ?';
+    const [result] = await dbConnection.execute(sql, [user.userName, user.userPassword]);
+    return result;
+};
+
+export async function uploadSong(song) {
+    let sql = 'INSERT INTO songs (songId, songName, songArtist, songAlbum, songGenre, songInstrument, songPath, songImage, songUploaderId, songUploadedAt) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const [result] = await dbConnection.execute(sql, [song.songName, song.songArtist, song.songAlbum, song.songGenre, song.songInstrument, song.songPath, song.songImage, song.songUploaderId, song.songUploadedAt]);
+    return result;
+}
+
+export async function downloadSong(songId) {
+    let sql = 'SELECT songPath FROM songs WHERE songId = ?';
+    const [result] = await dbConnection.execute(sql, [songId]);
+    return result;
+};
