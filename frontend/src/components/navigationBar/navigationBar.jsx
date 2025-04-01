b85c3b0b9083df19bb9d399ffa44356a94a14ae7
gengszterseges alper fatih <alperfatihcelik@gmail.com>
2025-04-01 23:18:10 +0200

    im aarondering it

--- frontend/src/components/navigationBar/navigationBar.jsx
+++ frontend/src/components/navigationBar/navigationBar.jsx
@@ -6,19 +6,13 @@ const Navbar = () => {
   return (
     <div className="navigationBar">
       <div className="browseButton">
-      <a href="#">
-        <img src="./src/assets/images/Logo.png" alt="" className="browseImage" />
-        <h3 className='stillA' id="browseText">Browse</h3>
-      </a>
+      <a href="#"><img src="./src/assets/images/Logo.png" alt="" className="browseImage" /></a>
       </div>
       <div className="plusButton">
       <a href="#"><img className='plusImage' src="./src/assets/images/Plus.png" alt="" /></a>
       </div>
       <div className="accountButton">
-        <a href="#">
-          <img src="./src/assets/images/Account.png" alt="" className="accountImage" />
-          <h3 className='stillA' id='accountText'>Account</h3>
-        </a>
+        <a href="#"><img src="./src/assets/images/Account.png" alt="" className="accountImage" /></a>
       </div>
     </div>
   );
