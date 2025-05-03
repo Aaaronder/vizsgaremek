208db09f9b37f6e42a758a10c8c242668935fcd0
Chen Junbo <77279251+chenjunbo730@users.noreply.github.com>
2025-05-04 00:14:48 +0200

    Add files via upload

---
+++ WinForm/MusicPlayerWinForms/Program.cs
@@ -0,0 +1,12 @@
+namespace MusicPlayerWinForms
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
---
+++ WinForm/MusicPlayerWinForms/Song.cs
@@ -0,0 +1,7 @@
+﻿public class Song
+{
+    public string FilePath { get; init; }
+    public string Title { get; init; }
+    public string Artist { get; init; }
+    public TimeSpan Duration { get; init; }
+}
